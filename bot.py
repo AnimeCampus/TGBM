3eb5e8dd3f8be7b5a41dff8a91d964cd75c4f0f1
PyroUserBot <129763781+PyroUserBot@users.noreply.github.com>
2023-08-15 16:52:40 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ current_player_index = 0
 board_size = 10
 board = [0] * (board_size * board_size)
 colors = ["red", "green", "blue", "yellow"]
-
+conversation_states = {}
 snakes = {16: 6, 47: 26, 49: 11, 56: 53, 62: 19, 64: 60, 87: 24, 93: 73, 95: 75, 98: 78}
 ladders = {1: 38, 4: 14, 9: 31, 21: 42, 28: 84, 36: 44, 51: 67, 71: 91, 80: 100}
 win_position = 100
